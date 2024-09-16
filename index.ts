9817c3815824e261a2aff595be63dbe9d3bfe09a
fusion-arc-e2e-test <115598311+fusion-arc-e2e-test@users.noreply.github.com>
2024-09-16 14:26:38 +1000

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 var http = require("http");
 
 http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.writeHead(200, {Content-Type: text/plain});
   response.write("Hello World. This page is running Node.js version: ");
   response.write(process.version);
   response.end();
