1979446acd65c9586b7fd0a64a3c65ca9c152f5a
fusion-arc-e2e-test <115598311+fusion-arc-e2e-test@users.noreply.github.com>
2024-09-16 13:14:43 +1000

    Lets see if AR catches this

--- index.ts
+++ index.ts
@@ -4,5 +4,4 @@ http.createServer(function(request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
   response.write("Hello World. This page is running Node.js version: ");
   response.write(process.version);
-  response.end();
 }).listen(8888);
