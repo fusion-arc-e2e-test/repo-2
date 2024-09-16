21d72a8607986f3ff1487c51478af3f3fbfb94a8
fusion-arc-e2e-test <115598311+fusion-arc-e2e-test@users.noreply.github.com>
2024-09-16 13:18:48 +1000

    Pushing a code with errors

--- index.ts
+++ index.ts
@@ -4,4 +4,5 @@ http.createServer(function(request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
   response.write("Hello World. This page is running Node.js version: ");
   response.write(process.version);
+  response.
 }).listen(8888);
